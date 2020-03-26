9cb19ac6aaddda90cc4f34e4dfddf29fb35654e0
Fixer <fixer3838@live.fr>
2020-03-26 22:08:15 +0100

    MODIFIED functions to be methods inside the ADDED Interpreter class

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,71 @@ use std::fs;
 use std::io;
 use std::io::Write;
 
-fn run_file(filename: &String) {
-    let path = Path::new(&filename);
-    let content = fs::read_to_string(path)
-        .expect("Error reading the file");
-    run(content);
+struct Interpreter {
+    has_error: bool
 }
 
-fn run_prompt() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    loop {
-        writeln!(&mut stdout, "> ").expect("Failed to get stdout");
-        stdout.flush().expect("Failed to flush the stdout content");
-
-        let mut input = String::new();
-        stdin.read_line(&mut input).expect("Failed to read line");
-        if input == "exit\n" {
-            break;
+impl Interpreter {
+    fn new() -> Interpreter {
+        Interpreter {
+            has_error: false,
         }
-        run(input);
     }
-}
 
-fn run(tokens: String) {
-    for token in tokens.chars() {
-        println!("{}", token);
+    fn get_run_type(&mut self) {
+        let args: Vec<String> = env::args().collect();
+        if args.len() > 2 {
+            println!("Usage panther [file]");
+        }
+        else if args.len() == 2 {
+            self.run_file(&args[1]);
+        }
+        else {
+            self.run_prompt();
+        }
     }
-}
 
-fn error(line: i32, message: String) {
-    report(line, String::from(""), message);
-}
-
-fn report(line: i32, location: String, message: String) {
-    println!("line: {} Error {}: {}", line, location, message);
-    let had_error = true;
+    fn run_file(&mut self, filename: &String) {
+        let path = Path::new(&filename);
+        let content = fs::read_to_string(path)
+            .expect("Error reading the file");
+        self.run(content);
+    }
+    
+    fn run_prompt(&mut self) {
+        let stdin = io::stdin();
+        let mut stdout = io::stdout();
+        loop {
+            writeln!(&mut stdout, "> ").expect("Failed to get stdout");
+            stdout.flush().expect("Failed to flush the stdout content");
+    
+            let mut input = String::new();
+            stdin.read_line(&mut input).expect("Failed to read line");
+            if input == "exit\n" {
+                break;
+            }
+            self.run(input);
+        }
+    }
+    
+    fn run(&mut self, tokens: String) {
+        for token in tokens.chars() {
+            println!("{}", token);
+        }
+    }
+    
+    fn error(&mut self, line: i32, message: String) {
+        self.report(line, String::from(""), message);
+    }
+    
+    fn report(&mut self, line: i32, location: String, message: String) {
+        println!("line: {} Error {}: {}", line, location, message);
+        self.has_error = true;
+    }
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 2 {
-        println!("Usage panther [file]");
-    }
-    else if args.len() == 2 {
-        run_file(&args[1]);
-    }
-    else {
-        run_prompt();
-    }
+    let mut panther = Interpreter::new();
+    panther.get_run_type();
 }
 

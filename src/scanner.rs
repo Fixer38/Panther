8c6a9024bd7350053b9b07aa576d7fcc16cf0007
Fixer <fixer3838@live.fr>
2020-04-06 21:37:19 +0200

    ADDED helper methods for future scanToken

--- src/scanner.rs
+++ src/scanner.rs
@@ -4,15 +4,15 @@ use super::token_type::TokenType;
 pub struct Scanner<T> {
     source: String,
     tokens: Vec<Token<T>>,
-    start: i32,
-    current: i32,
-    line: i32
+    start: usize,
+    current: usize,
+    line: usize
 }
 
 impl<T> Scanner<T> {
     fn new(source: String) -> Scanner<T> {
         Scanner {
-            source: source,
+            source,
             tokens: vec![],
             start: 0,
             current: 0,
@@ -20,12 +20,24 @@ impl<T> Scanner<T> {
         }
     }
 
-    fn is_at_end(&mut self) -> bool {
-        return self.current >= self.source.len() as i32
-    }
 
     fn scan_token(&mut self) {
         println!("to be done tomorrow");
+    }
+
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        let source_vec = self.source.chars().nth(self.current - 1).unwrap();
+        source_vec
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        let text = &self.source[self.start..self.current];
+        self.tokens.push(Token { token_type, lexeme: text.to_string(), literal: None, line: self.line });
+    }
+
+    fn is_at_end(&mut self) -> bool {
+        return self.current >= self.source.len()
     }
 
     fn scan_tokens(&mut self) -> &Vec<Token<T>> {
@@ -33,7 +45,7 @@ impl<T> Scanner<T> {
             self.start = self.current;
             self.scan_token();
         }
-        self.tokens.push(Token { type_token: TokenType::EOF, lexeme: String::from(""), literal: None, line: self.line });
+        self.tokens.push(Token { token_type: TokenType::EOF, lexeme: String::from(""), literal: None, line: self.line });
         &self.tokens
     }
 }
